93f77ce3e055d82a79b95b22d05f61c079b9e9a6
Srikanth B <srikkanthb@yahoo.com>
2024-11-24 15:11:26 -0500

    test1

--- src/main/java/com/techprimers/springbatchexample1/batch/Processor.java
+++ src/main/java/com/techprimers/springbatchexample1/batch/Processor.java
@@ -14,11 +14,16 @@ public class Processor implements ItemProcessor<User, User> {
     private static final Map<String, String> DEPT_NAMES =
             new HashMap<>();
 
+            
+
     public Processor() {
         DEPT_NAMES.put("001", "Technology");
         DEPT_NAMES.put("002", "Operations");
         DEPT_NAMES.put("003", "Accounts");
     }
+
+
+
 
     @Override
     public User process(User user) throws Exception {
